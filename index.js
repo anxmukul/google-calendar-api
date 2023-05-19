fadfea7d50e7a3f300146f83f4ff745be2a97413
anxmukul <kmukul1197@gmail.com>
2023-05-19 12:25:56 +0530

    Installing googleapis and starting server at port:3000

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("Hello There");
+const express = require('express')
+
+const google = require('googleapis')
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello World this is Google calendar API Integration with nodejs')
+})
+
+app.listen(port, ()=>{
+    console.log(`Example app listening at port: ${port}`);
+})
