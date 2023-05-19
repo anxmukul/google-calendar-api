0ef14e8a36dce793db31abebe0bf1c9602d31122
anxmukul <kmukul1197@gmail.com>
2023-05-19 17:56:22 +0530

    Sending response in json format

--- index.js
+++ index.js
@@ -1,93 +1,67 @@
-const express = require('express')
-const dotenv = require('dotenv')
-const google = require('googleapis');
+const express = require("express");
+const dotenv = require("dotenv");
+const google = require("googleapis");
 dotenv.config({});
-
 
 const app = express();
 const port = 3000;
 
 const oauth2Client = new google.Auth.OAuth2Client(
-    process.env.CLIENT_ID,
-    process.env.CLIENT_SECRET,
-    process.env.REDIRECT_URL
-)
+  process.env.CLIENT_ID,
+  process.env.CLIENT_SECRET,
+  process.env.REDIRECT_URL
+);
 
-const scopes = ['https://www.googleapis.com/auth/calendar']
-
-/*
-async function getEvents() {
-    const calendar = google.google.calendar({
-        version: 'v3',
-        auth: oauth2Client
-    });
-    const res = await calendar.events.list({
-      calendarId: 'primary',
-      timeMin: new Date().toISOString(),
-      maxResults: 10,
-      singleEvents: true,
-      orderBy: 'startTime',
-    });
-    const events = res.data.items;
-    if (!events || events.length === 0) {
-      console.log('No events found.');
-      return;
-    }
-    console.log('Events are:-------> ');
+const scopes = ["https://www.googleapis.com/auth/calendar"];
 
 
-    events.map((event, i) => {
-      const start = event.start.dateTime || event.start.date;
-      console.log(`${ start} - ${ event.summary}`);
-    });
+app.get("/rest/v1/calendar/init/", (req, res) => {
+  const url = oauth2Client.generateAuthUrl({
+    access_type: "offline",
+    scope: scopes,
+  });
+  res.redirect(url);
+});
 
+app.get("/rest/v1/calendar/redirect/", async (req, res) => {
+  const code = req.query.code;
+
+  const { tokens } = await oauth2Client.getToken(code);
+  oauth2Client.setCredentials(tokens);
+  const calendar = google.google.calendar({
+    version: "v3",
+    auth: oauth2Client,
+  });
+  const result = await calendar.events.list({
+    calendarId: "primary",
+    timeMin: new Date().toISOString(),
+    maxResults: 10,
+    singleEvents: true,
+    orderBy: "startTime",
+  });
+  const events = result.data.items;
+
+  // Creating json to return events summary and time
+  var jsonObj = {}; // empty Object
+  var key = "Events";
+  jsonObj[key] = []; // empty Array, which we can push() values into
+  if (!events || events.length === 0) {
+    console.log("No events found.");
+    return;
   }
-  */
-
-
-
-app.get('/rest/v1/calendar/init/', (req, res) => {
-
-    const url = oauth2Client.generateAuthUrl({
-        access_type : "offline",
-        scope : scopes
-    })
-    res.redirect(url)
-})
-
-app.get('/rest/v1/calendar/redirect/', async (req, res) => {
-
-    const code = req.query.code;
-
-    const {tokens} = await oauth2Client.getToken(code)
-    oauth2Client.setCredentials(tokens);
-    const calendar = google.google.calendar({
-        version: 'v3',
-        auth: oauth2Client
-    });
-    const result = await calendar.events.list({
-      calendarId: 'primary',
-      timeMin: new Date().toISOString(),
-      maxResults: 10,
-      singleEvents: true,
-      orderBy: 'startTime',
-    });
-    const events = result.data.items;
-
-    if (!events || events.length === 0) {
-      console.log('No events found.');
-      return;
+//   console.log("Events are:-------> ");
+  events.map((event, i) => {
+    const start = event.start.dateTime || event.start.date;
+    // console.log(`${start} - ${event.summary}`);
+    var tempobj = {
+        "startTime": start,
+        "Summary": event.summary
     }
-    console.log('Events are:-------> ');
-    events.map((event, i) => {
-      const start = event.start.dateTime || event.start.date;
-      console.log(`${ start} - ${ event.summary}`);
-    });
-    res.send(events);
+    jsonObj[key].push(tempobj);
+  });
+  res.send(jsonObj);
+});
 
-})
-
-app.listen(port, ()=>{
-    console.log(`Example app listening at port: ${port}`);
-})
-
+app.listen(port, () => {
+  console.log(`Example app listening at port: ${port}`);
+});
