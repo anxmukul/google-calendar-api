3a4df3b8e770fde0d290699703d0664d8716ddd5
anxmukul <kmukul1197@gmail.com>
2023-05-19 13:22:02 +0530

    Creating env file to add client_id, secret_key, and creating scope

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
 const express = require('express')
-
+const dotenv = require('dotenv')
 const google = require('googleapis')
 
 const app = express();
 const port = 3000;
+dotenv.config({});
 
-app.get('/', (req, res) => {
-    res.send('Hello World this is Google calendar API Integration with nodejs')
+const oauth2Client = new google.Auth.OAuth2Client(
+    process.env.CLIENT_ID,
+    process.env.CLIENT_SECRET,
+    process.env.REDIRECT_URL
+)
+
+const scopes = ['https://www.googleapis.com/auth/calendar']
+
+app.get('/rest/v1/calendar/init/', (req, res) => {
+
+    const url = oauth2Client.generateAuthUrl({
+        access_type : "offline",
+        scope : scopes
+    })
+    res.redirect(url)
+    // res.send('Hello World this is Google calendar API Integration with nodejs')
+})
+
+app.get('/rest/v1/calendar/redirect/', (req, res) => {
+    res.send("Got access to google calender");
 })
 
 app.listen(port, ()=>{
